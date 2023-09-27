5215bf1ccd08ecaae9e8620469e75b4b763ff377
ernaldewi <erna.listianadewi@gmail.com>
2023-09-27 17:25:29 +0700

    Create index.html and add some JS + CSS + assets

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// NAVBAR MENU BUTTON
+const navbarNav = document.querySelector(".navbar-nav");
+const menuButton = document.querySelector(".menu");
+const menuImg = document.querySelector(".menu-img");
+const overlay = document.querySelector(".overlay");
+const body = document.body;
+
+function updateVisibility() {
+  const visibility = navbarNav.getAttribute("data-visible");
+  const newVisibility = visibility === "false" ? "true" : "false";
+
+  navbarNav.setAttribute("data-visible", newVisibility);
+  menuButton.setAttribute("aria-expanded", newVisibility);
+  if (newVisibility === "false") {
+    menuImg.src = "assets/svg/menu.svg";
+  } else {
+    menuImg.src = "assets/svg/close.svg";
+  }
+  overlay.setAttribute("data-visible", newVisibility);
+}
+
+menuButton.addEventListener("click", () => {
+  updateVisibility();
+});
+
+overlay.addEventListener("click", () => {
+  updateVisibility();
+});
+
+// ERNA - CAROUSEL SERVIVE
+const carousel = document.querySelector(".carousel");
+const services = document.querySelectorAll(".service");
+const serviceCount = services.length;
+
+let currentIndex = 0;
+const intervalTime = 5000; // Change slide every 5 seconds
+
+function nextService() {
+  services[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + 1) % serviceCount;
+  services[currentIndex].classList.add("active");
+  updateCarousel();
+}
+
+function updateCarousel() {
+  const translateXValue = -currentIndex * 100;
+  carousel.style.transform = `translateX(${translateXValue}%)`;
+}
+
+setInterval(nextService, intervalTime);
