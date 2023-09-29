9b926279d55ad3efc547acbff4972be5a174af8e
Rafaell Victor Christian <rafaellvictor3@gmail.com>
2023-09-29 16:04:44 +0700

    insert js Ara ke main.js

--- js/main.js
+++ js/main.js
@@ -65,59 +65,124 @@ labels.forEach(function (label) {
   });
 });
 
-
 //list doctor
 
 const doctorData = [
-  { name: "drg. Wisis Arif Setiawan K D, Sp.KGA (Pedodonti/Gigi Anak)", schedule: ["Senin - Rabu : 09.00 - 14.00", "Kamis - Sabtu : 15.00 - 19.00"] },
-  { name: "drg. Zahra Maysani", schedule: ["Senin - Rabu : 09.00 - 14.00", "Kamis - Sabtu : 15.00 - 19.00"] },
-  { name: "drg. Erna Listiana Dewi, MPH", schedule: ["Senin - Rabu : 09.00 - 14.00", "Kamis - Sabtu : 15.00 - 19.00"]},
-  { name: "drg. Rafaell Victor Christian, Sp.KG", schedule: ["Senin - Rabu : 15.00 - 19.00", "Kamis - Sabtu : 09.00 - 14.00"]},
-  { name: "drg. Andhika Hanif Prasetyo K Sp.KGA", schedule: ["Senin - Rabu : 15.00 - 19.00", "Kamis - Sabtu : 09.00 - 14.00"]},
-  { name: "drg. Anggita Prameswari K Sp.KGA", schedule: ["Senin - Rabu : 15.00 - 19.00", "Kamis - Sabtu : 09.00 - 14.00"]}
+  {
+    name: "drg. Wisis Arif Setiawan K D, Sp.KGA (Pedodonti/Gigi Anak)",
+    schedule: ["Senin - Rabu : 09.00 - 14.00", "Kamis - Sabtu : 15.00 - 19.00"],
+  },
+  {
+    name: "drg. Zahra Maysani",
+    schedule: ["Senin - Rabu : 09.00 - 14.00", "Kamis - Sabtu : 15.00 - 19.00"],
+  },
+  {
+    name: "drg. Erna Listiana Dewi, MPH",
+    schedule: ["Senin - Rabu : 09.00 - 14.00", "Kamis - Sabtu : 15.00 - 19.00"],
+  },
+  {
+    name: "drg. Rafaell Victor Christian, Sp.KG",
+    schedule: ["Senin - Rabu : 15.00 - 19.00", "Kamis - Sabtu : 09.00 - 14.00"],
+  },
+  {
+    name: "drg. Andhika Hanif Prasetyo K Sp.KGA",
+    schedule: ["Senin - Rabu : 15.00 - 19.00", "Kamis - Sabtu : 09.00 - 14.00"],
+  },
+  {
+    name: "drg. Anggita Prameswari K Sp.KGA",
+    schedule: ["Senin - Rabu : 15.00 - 19.00", "Kamis - Sabtu : 09.00 - 14.00"],
+  },
 
   // Tambahkan data dokter dan jadwal lainnya di sini
 ];
 // Fungsi untuk menampilkan jadwal dokter
-    function showDoctorSchedule(index) {
-            const schedule = doctorData[index].schedule;
-            const jadwalContainer = document.getElementById("jadwal-dokter");
-            const namaDokter = document.getElementById("nama-dokter");
+function showDoctorSchedule(index) {
+  const schedule = doctorData[index].schedule;
+  const jadwalContainer = document.getElementById("jadwal-dokter");
+  const namaDokter = document.getElementById("nama-dokter");
 
-            // Menampilkan nama dokter di samping h3 "Jadwal Dokter"
-            namaDokter.textContent = doctorData[index].name;
+  // Menampilkan nama dokter di samping h3 "Jadwal Dokter"
+  namaDokter.textContent = doctorData[index].name;
 
-            jadwalContainer.innerHTML = ""; // Menghapus semua elemen di dalam jadwalContainer sebelum menambahkan yang baru
+  jadwalContainer.innerHTML = ""; // Menghapus semua elemen di dalam jadwalContainer sebelum menambahkan yang baru
 
-            schedule.forEach((data) => {
-                const listschedul = document.createElement("p");
-                listschedul.textContent = data;
+  schedule.forEach((data) => {
+    const listschedul = document.createElement("p");
+    listschedul.textContent = data;
 
-                // Menambahkan event listener untuk menghapus elemen saat diklik
-                listschedul.addEventListener("click", function () {
-                    jadwalContainer.removeChild(listschedul);
-                });
+    // Menambahkan event listener untuk menghapus elemen saat diklik
+    listschedul.addEventListener("click", function () {
+      jadwalContainer.removeChild(listschedul);
+    });
 
-                jadwalContainer.appendChild(listschedul);
-            });
-        }
-    
-    
+    jadwalContainer.appendChild(listschedul);
+  });
+}
 
+// Fungsi untuk mengisi daftar dokter
+function populateDoctorList() {
+  const doctorList = document.getElementById("doctor-list");
+  doctorData.forEach((doctor, index) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = doctor.name;
+    listItem.addEventListener("click", () => showDoctorSchedule(index));
+    doctorList.appendChild(listItem);
+  });
+}
 
-    // Fungsi untuk mengisi daftar dokter
-    function populateDoctorList() {
-      const doctorList = document.getElementById("doctor-list");
-      doctorData.forEach((doctor, index) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = doctor.name;
-        listItem.addEventListener("click", () => showDoctorSchedule(index));
-        doctorList.appendChild(listItem);
-      });
+// Panggil fungsi untuk mengisi daftar dokter saat halaman dimuat
+window.addEventListener("load", populateDoctorList);
+
+// Punya Ara
+
+document.addEventListener("DOMContentLoaded", function () {
+  const registrationForm = document.getElementById(".register-container");
+  const usernameInput = document.getElementById("username");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+
+  registrationForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    const email = emailInput.value;
+
+    // Validasi password dan konfirmasi password
+    if (password !== confirmPassword) {
+      alert("Password dan konfirmasi password tidak cocok.");
+      return;
     }
 
-    // Panggil fungsi untuk mengisi daftar dokter saat halaman dimuat
-    window.addEventListener("load", populateDoctorList);
+    // Validasi panjang minimal password (minimal 8 karakter)
+    if (password.length < 8) {
+      alert("Password harus memiliki minimal 8 karakter.");
+      return;
+    }
 
+    // Validasi penggunaan karakter khusus dalam password
+    if (!/[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(password)) {
+      alert("Password harus mengandung setidaknya satu karakter khusus.");
+      return;
+    }
 
+    // Validasi penggunaan huruf besar (uppercase) dalam password
+    if (!/[A-Z]/.test(password)) {
+      alert(
+        "Password harus mengandung setidaknya satu huruf besar (uppercase)."
+      );
+      return;
+    }
 
+    // Validasi penggunaan huruf kecil (lowercase) dalam password
+    if (!/[a-z]/.test(password)) {
+      alert(
+        "Password harus mengandung setidaknya satu huruf kecil (lowercase)."
+      );
+      return;
+    }
+
+    // Lakukan pendaftaran pengguna atau kirim data ke server sesuai logika aplikasi Anda
+    alert("Registrasi berhasil!");
+  });
+});
